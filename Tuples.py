11863949f92d9a8ccb62ff8b6479f40eb9d2b1b5
Radhika-nayak12 <76205547+Radhika-nayak12@users.noreply.github.com>
2024-09-25 00:06:53 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,40 @@
+# Tuple are used to store multiple items in a single variable.
+# Tuple items are ordered, unchangable, and allow duplicate value
+# Tuples are written with round brackets.
+# Mutable
+# thislist = ("apple", "banana", "Cherry")
+# print(thislist)
+
+# thislist = ("apple", "banana", "Cherry")
+# print(len(thislist))
+
+# tuple1 = ("apple","banana")
+# tuple2 = (1,2,3)
+# tuple3 = (True,False,True)
+# tuple4 = ("abc",True,30,"male",40)
+# mytuple = ("apple","banana","cherry")
+# print(type(mytuple))
+
+# thistuple = ("apple","banana","cherry")
+# print(thistuple[1])
+
+# thistuple = ("apple","banana","cherry")
+# print(thistuple[-1])
+
+# thistuple = ("apple","banana","cherry","orange","kiwi","melon","mango")
+# print(thistuple[2:5])
+
+# thistuple = ("apple","banana","cherry","orange","kiwi","melon","mango")
+# print(thistuple[:4])
+
+
+# x = ("apple","banana","cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
+# print(x)
+
+# you cannot change its values. Tuple are unchangable or immutable as it also called
+# add
+# remove 
+# del
